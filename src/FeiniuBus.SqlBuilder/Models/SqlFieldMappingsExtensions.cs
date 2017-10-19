097a32ad7376978fb358d561aec06481ca2b2917
Andrew Cui <mykge@outlook.com>
2017-10-19 15:10:22 +0800

    build fix

---
+++ src/FeiniuBus.SqlBuilder/Models/SqlFieldMappingsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FeiniuBus.SqlBuilder
+{
+    public static class SqlFieldMappingsExtensions
+    {
+        public static SqlFieldMappings Map(this SqlFieldMappings mappings, string key, string sqlfield, Type type = null)
+        {
+            mappings.Add(key, sqlfield, type);
+            return mappings;
+        }
+    }
+}
